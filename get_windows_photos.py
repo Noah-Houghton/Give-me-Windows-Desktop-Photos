2cc4b9b7d95baf014d8648591c79d8dbe2e78fa4
Noah Houghton <9970944+Noah-Houghton@users.noreply.github.com>
2022-07-31 16:31:35 -0400

    use pillow for image size

--- get_windows_photos.py
+++ get_windows_photos.py
@@ -3,7 +3,7 @@ import filecmp
 import os
 import requests
 from bs4 import BeautifulSoup
-import tempfile
+from PIL import Image
 import yaml
 from yaml import CLoader as Loader
 
@@ -90,24 +90,8 @@ def get_image_name(filePath):
     raise(e)
 
 def getDimension(filename):
-   # open image for reading in binary mode
-   with open(filename,'rb') as img_file:
-    # height of image (in 2 bytes) is at 164th position
-    img_file.seek(163)
-
-    # read the 2 bytes
-    a = img_file.read(2)
-
-    # calculate height
-    height = (a[0] << 8) + a[1]
-
-    # next 2 bytes is width
-    a = img_file.read(2)
-
-    # calculate width
-    width = (a[0] << 8) + a[1]
-
-    return [width, height]
+  im = Image.open(filename)
+  return im.size
 
 if __name__ == "__main__":
   get_images()
